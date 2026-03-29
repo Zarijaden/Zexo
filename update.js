12651ec2dd661a17175280c4543899420abeb9f9
Zarijaden <Zarijaden@163.com>
2026-03-29 15:11:26 +0800

    Update version check for Zexo to 0.2.0

--- update.js
+++ update.js
@@ -27,14 +27,14 @@ var ajax = ajaxObject();
     ajax.send();
 }
 
-if(zexo_ver == 'Zexo@2.0'){
+if(zexo_ver == 'Zexo@0.2.0'){
   swal({
 title: "成功",
     text: "无需更新，当前已是最新版本",
     icon: "success",
 });
 }else{
-swal("存在2.0更新，是否更新？", {
+swal("存在0.2.0更新，是否更新？", {
   icon: "warning",
   buttons: {
     cancel: "否",

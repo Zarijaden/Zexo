1422780a01843c961650b33b906abf2b60021a20
Zarijaden <Zarijaden@163.com>
2026-04-02 19:46:24 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -568,6 +568,8 @@ async function handleRequest(request) {
           </div>
         </div>
       </div>`
+<script src="node_modules/eruda/eruda.js"></script>
+<script>eruda.init();</script>
               hpp_js = `<script src='https://cdn.jsdelivr.net/gh/HexoPlusPlus/HexoPlusPlus@${hpp_CDNver}/tool.js'></script>`
             }
             if (path == "/hpp/admin/dash/set") {
